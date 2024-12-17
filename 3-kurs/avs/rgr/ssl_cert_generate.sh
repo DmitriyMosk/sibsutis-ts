46a8c335910bdc94e83fe226e21bd7be470dc9d5
Dmitry <94410956z@gmail.com>
2024-12-18 03:03:01 +0700

    sync commit

--- 3-kurs/avs/rgr/ssl_cert_generate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh 
-
-mkdir ssl && cd ssl
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-    -keyout jenkins.key -out jenkins.crt \
-    -subj "/C=RU/ST=Russia/L=Novosibirsk/O=SibsutisAVS/OU=Unit/CN=localhost"
-cd ..
